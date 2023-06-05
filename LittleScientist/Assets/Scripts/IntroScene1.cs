51722d1d969bdd6ea1c7ef5cc8a9b0fecbf47b16
aditya.digimantra <adityapandey0264@gmail.com>
2023-06-05 12:13:58 +0530

    Created Splash Screen

---
+++ LittleScientist/Assets/Scripts/IntroScene1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScene1 : MonoBehaviour
+{
+    public int SceneNumbertoLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(startSplashScreen());
+    }
+
+    IEnumerator startSplashScreen()
+    {
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(SceneNumbertoLoad);
+
+    }
+}
