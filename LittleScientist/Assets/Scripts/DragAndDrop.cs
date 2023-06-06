6e3f67f3f84a0b50af9dcf57cd8b2426c14741bb
aditya.digimantra <adityapandey0264@gmail.com>
2023-06-06 18:19:57 +0530

    Completed Drag and Drop for GameObjects [DragAndDrop].

--- LittleScientist/Assets/Scripts/DragAndDrop.cs
+++ LittleScientist/Assets/Scripts/DragAndDrop.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DragAndDrop : MonoBehaviour
+public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerDown(PointerEventData eventData)
     {
-        
+        canvasGroup.alpha = 0.6f; // Adjust the transparency of the image when dragging
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / GetCanvasScale();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1f; // Reset the transparency of the image when dragging ends
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    private float GetCanvasScale()
+    {
+        Canvas canvas = GetComponentInParent<Canvas>();
+        if (canvas != null)
+            return canvas.scaleFactor;
+        else
+            return 1f;
     }
 }
