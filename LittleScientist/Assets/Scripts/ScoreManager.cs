89d1874fa9d8cf3b3844441525c8e80382ee62f7
aditya.digimantra <aditya.pandey@digimantra.com>
2024-02-28 10:39:56 +0530

    Set No of Discovered Element in Discovery Tray for all Themes.

--- LittleScientist/Assets/Scripts/ScoreManager.cs
+++ LittleScientist/Assets/Scripts/ScoreManager.cs
@@ -6,8 +6,22 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour
 {
     public Text ScoreText;
-    public Text ElementDiscoveredText;
-    public Text TotalElementsText;
+
+    [Header("Def_Discovery Tray Data")]
+    public GameObject def_discoveryTray;
+    public Text def_ElementDiscoveredText;
+    public Text def_TotalElementsText;
+
+    [Header("for_Discovery Tray Data")]
+    public GameObject for_discoveryTray;
+    public Text for_ElementDiscoveredText;
+    public Text for_TotalElementsText;
+
+    [Header("aqua_Discovery Tray Data")]
+    public GameObject aqua_discoveryTray;
+    public Text aqua_ElementDiscoveredText;
+    public Text aqua_TotalElementsText;
+
     public combinationManager combinationManager;
     // Start is called before the first frame update
     void Start()
@@ -21,8 +35,22 @@ public class ScoreManager : MonoBehaviour
         if (combinationManager != null)
         {
             ScoreText.text = combinationManager.FinalScore.ToString();
-            ElementDiscoveredText.text = combinationManager.elementDiscoveredCount.ToString();
-            TotalElementsText.text = "/"+ combinationManager.TotalElements.ToString();
+            if(def_discoveryTray.activeSelf)
+            {
+                def_ElementDiscoveredText.text = combinationManager.elementDiscoveredCount.ToString();
+                def_TotalElementsText.text = "/" + combinationManager.TotalElements.ToString();
+            }
+            if(for_discoveryTray.activeSelf)
+            {
+                for_ElementDiscoveredText.text = combinationManager.elementDiscoveredCount.ToString();
+                for_TotalElementsText.text = "/" + combinationManager.TotalElements.ToString();
+            }
+            if(aqua_discoveryTray.activeSelf)
+            {
+                aqua_ElementDiscoveredText.text = combinationManager.elementDiscoveredCount.ToString();
+                def_TotalElementsText.text = "/" + combinationManager.TotalElements.ToString();
+            }
+            
         }
     }
 }
